d16aaecd78926045780ceeadba83f2c867863e60
porludom <porludom@mail.ru>
2022-04-11 13:58:09 +0300

    .

--- src/ru/innopolis/problem3/problem3.java
+++ src/ru/innopolis/problem3/problem3.java
@@ -1,2 +1,36 @@
+package ru.innopolis.problem3;
+
+import java.util.Iterator;
+
 // your solution here
 //fix all the errors including package errors
+class Item{
+
+}
+class Chest implements Iterator<Item>{
+    ItemEntry first;
+
+    @Override
+    public boolean hasNext() {
+        return first.next != null;
+    }
+
+    @Override
+    public Item next() {
+        Item t = first.val;
+        first = first.next;
+        return t;
+    }
+
+    static class ItemEntry{
+        Item val;
+        ItemEntry next;
+
+        public ItemEntry(Item val) {
+            this.val = val;
+        }
+        public void setNext(ItemEntry entry){
+            next = entry;
+        }
+    }
+}
